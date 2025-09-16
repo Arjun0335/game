88cd2b7a59cbc76d1518eab13873513df9dca5d5
Arjun Gupta <145846275+Arjun0335@users.noreply.github.com>
2025-09-17 01:26:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // local dev runs on http://localhost:3000
+  },
+  build: {
+    outDir: "dist", // Vercel will serve this folder
+  },
 });
